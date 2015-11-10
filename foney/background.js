de80847bc7f83f19533c8512ad321db56b22282c
Ben Anderson <ben.anderson@vendhq.com>
2015-11-10 21:41:58 +1300

    more sale tutuing. add example job page.

--- foney/background.js
+++ foney/background.js
@@ -3,10 +3,12 @@
 chrome.omnibox.onInputChanged.addListener(
   function(text, suggest) {
     console.log('inputChanged: ' + text);
+    /*
     suggest([
       {content: text + " one", description: "the first one"},
       {content: text + " number two", description: "the second entry"}
     ]);
+    */
   });
 
   // This event is fired with the user accepts the input in the omnibox.
@@ -21,56 +23,100 @@ chrome.omnibox.onInputChanged.addListener(
         console.log('callVend error. message: ' + errorMessage);
       });
 
-      createVendSale(text, function() {
-        console.log('createVendSale callback');
-        // what do we do here? send the sale?
+      var job = getJob(text);
 
+      // TODO: move inside the callback.
+      var sale = createSale(job);
+
+      postSale(sale, function() {
+        console.log('postSale callback');
+        console.log(sale);
       }, function(errorMessage) {
-        console.log('callVend error. message: ' + errorMessage);
+        console.log('postSale error. message: ' + errorMessage);
       });
 
-
-      getImageUrl(text, function(imageUrl, width, height) {
+      /*getImageUrl(text, function(imageUrl, width, height) {
         console.log('Callback for getImageUrl. Search term: ' + text + '\n' +
         'Google image search result: ' + imageUrl);
         console.log('width: ' + width + '. height: ' + height + '. if you care.');
       }, function(errorMessage) {
         console.log('Cannot display image. ' + errorMessage);
-      });
-
+      });*/
     });
 
-    function createVendSale(jobNumber, callback, errorCallback) {
+    function getJob(jobNumber, callback, errorCallback) {
+      console.log('getJob(). jobNumber: ' + jobNumber);
+      // TODO: call RepairCMS
+      return {'job': 'some fake job'};
+    }
 
-      console.log('createVendSale: ' + jobNumber);
+    function createSale(job) {
+      // TODO: get a real job!
+      var sale = {
+         "register_id": "31eb0866-e756-11e5-fed9-8136c14c4177",
+         "sale_date": "2015-10-14 00:06:15", // TODO: will it default to now?
+         "user_id": "31eb0866-e756-11e5-fed9-8136c14db57d",
+         "total_price": 100.00,
+         "total_tax":10,
+         "tax_name": "GST",
+         // TODO: change to PARKED
+         "status": "LAYBY",
+         "note": null,
+         "line_items": [
+           {
+             // APPLE IPHONE 30 PIN DATA CABLE
+             "product_id": "31eb0866-e75f-11e5-fed9-84e950e7626a",
+             "unit_price": 100.00,
+             "quantity": 1,
+             // TODO: what does this mean? true? i do want to set the price.
+             "price_set": false,
+             /*
+             "tax_components": [
+               {
+                 "rate_id": "080027db-dc32-11e5-fe35-721c2068d021",
+                 "total_tax": 3
+               },
+               {"rate_id": "080027db-dc32-11e5-fe35-721c1b38eb2a",
+                 "total_tax": 5
+                 }
+             ],
+             */
+             "sequence": 0,
+             "note": "The line item note",
+             "status": "CONFIRMED"
+           }
+         ]
+      };
+
+      return sale;
+    }
+
+    // TODO: function getJob()
+
+    function postSale(sale, callback, errorCallback) {
+      console.log('postSale()');
+      console.log(sale);
 
       var baseUrl = 'https://fonekingdemo.vendhq.com';
       var url = baseUrl + '/api/2.0/sales';
-
       var x = new XMLHttpRequest();
+      // x.setRequestHeader('Content-Type', 'application/json');
+      // x.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+      // x.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+      // x.Content-Type = 'application/json';
       x.open('POST', url);
+
       x.responseType = 'json';
-
-      // x.open("POST", "/json-handler");
-      x.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
-      // x.send(JSON.stringify({name:"John Rambo", time:"2pm"}));
-
-      var sale = {
-          'product_id': '1234'
-      };
 
       x.onload = function() {
         var response = x.response;
-
+        var resp = JSON.parse(x.responseText);
         var yolo = 'x';
-
-        /*
-        var data = response.data;
+        /*var data = response.data;
         for (i = 0; i < data.length; i++) {
             var product = data[i];
             console.log('Product: ' + product.name);
-        }
-        */
+        }*/
       }
 
       x.onerror = function() {
@@ -95,9 +141,8 @@ chrome.omnibox.onInputChanged.addListener(
         var data = response.data;
         for (i = 0; i < data.length; i++) {
             var product = data[i];
-            console.log('Product: ' + product.name);
+            // console.log('Product: ' + product.name);
         }
-
         /*
         todo: if no data
           errorCallback('No response from' + url);
