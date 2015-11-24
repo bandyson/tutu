ca3026eb0aec059f91ac31c7084bbf4074e551fa
Ben Anderson <ben.anderson@vendhq.com>
2015-11-24 20:33:36 +1300

    quick tidy up

--- foney/background.js
+++ foney/background.js
@@ -11,17 +11,16 @@ chrome.omnibox.onInputChanged.addListener(
     */
   });
 
-  // This event is fired with the user accepts the input in the omnibox.
+  // This event is fired when the user accepts the input in the omnibox.
   chrome.omnibox.onInputEntered.addListener(
     function(text) {
       console.log('inputEntered: ' + text);
-      // alert('You just typed "' + text + '"');
 
-      callVend(text, function() {
+      /*callVend(text, function() {
         console.log('callVend callback');
       }, function(errorMessage) {
         console.log('callVend error. message: ' + errorMessage);
-      });
+      });*/
 
       var job = getJob(text);
 
@@ -34,14 +33,6 @@ chrome.omnibox.onInputChanged.addListener(
       }, function(errorMessage) {
         console.log('postSale error. message: ' + errorMessage);
       });
-
-      /*getImageUrl(text, function(imageUrl, width, height) {
-        console.log('Callback for getImageUrl. Search term: ' + text + '\n' +
-        'Google image search result: ' + imageUrl);
-        console.log('width: ' + width + '. height: ' + height + '. if you care.');
-      }, function(errorMessage) {
-        console.log('Cannot display image. ' + errorMessage);
-      });*/
     });
 
     function getJob(jobNumber, callback, errorCallback) {
@@ -52,7 +43,7 @@ chrome.omnibox.onInputChanged.addListener(
 
     function createSale(job) {
       // TODO: get a real job!
-      var sale = {
+      return {
          "register_id": "31eb0866-e756-11e5-fed9-8136c14c4177",
          "sale_date": "2015-10-14 00:06:15", // TODO: will it default to now?
          "user_id": "31eb0866-e756-11e5-fed9-8136c14db57d",
@@ -87,8 +78,6 @@ chrome.omnibox.onInputChanged.addListener(
            }
          ]
       };
-
-      return sale;
     }
 
     // TODO: function getJob()
@@ -97,9 +86,11 @@ chrome.omnibox.onInputChanged.addListener(
       console.log('postSale()');
       console.log(sale);
 
-      var baseUrl = 'https://fonekingdemo.vendhq.com';
+      // var baseUrl = 'https://fonekingdemo.vendhq.com';
+      var baseUrl = 'https://local-demo.dev.vendhq.localdomain/';
       var url = baseUrl + '/api/2.0/sales';
       var x = new XMLHttpRequest();
+
       // x.setRequestHeader('Content-Type', 'application/json');
       // x.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
       // x.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
@@ -126,7 +117,7 @@ chrome.omnibox.onInputChanged.addListener(
       x.send(JSON.stringify(sale));
     }
 
-    function callVend(sku, callback, errorCallback) {
+    function  callVend(sku, callback, errorCallback) {
       console.log('callVend: ' + sku);
 
       var baseUrl = 'https://fonekingdemo.vendhq.com';
@@ -155,47 +146,4 @@ chrome.omnibox.onInputChanged.addListener(
       };
 
       x.send();
-    }
-
-    /**
-    * @param {string} searchTerm - Search term for Google Image search.
-    * @param {function(string,number,number)} callback - Called when an image has
-    *   been found. The callback gets the URL, width and height of the image.
-    * @param {function(string)} errorCallback - Called when the image is not found.
-    *   The callback gets a string that describes the failure reason.
-    */
-    function getImageUrl(searchTerm, callback, errorCallback) {
-      // Google image search - 100 searches per day.
-      // https://developers.google.com/image-search/
-      console.log('ben was here: ' + searchTerm);
-
-      var searchUrl = 'https://ajax.googleapis.com/ajax/services/search/images' +
-      '?v=1.0&q=' + encodeURIComponent(searchTerm);
-      var x = new XMLHttpRequest();
-      x.open('GET', searchUrl);
-      // The Google image search API responds with JSON, so let Chrome parse it.
-      x.responseType = 'json';
-      x.onload = function() {
-        // Parse and process the response from Google Image Search.
-        var response = x.response;
-        if (!response || !response.responseData || !response.responseData.results ||
-          response.responseData.results.length === 0) {
-            errorCallback('No response from Google Image search!');
-            return;
-          }
-          var firstResult = response.responseData.results[0];
-          // Take the thumbnail instead of the full image to get an approximately
-          // consistent image size.
-          var imageUrl = firstResult.tbUrl;
-          var width = parseInt(firstResult.tbWidth);
-          var height = parseInt(firstResult.tbHeight);
-          console.assert(
-            typeof imageUrl == 'string' && !isNaN(width) && !isNaN(height),
-            'Unexpected respose from the Google Image Search API!');
-            callback(imageUrl, width, height);
-          };
-          x.onerror = function() {
-            errorCallback('Network error.');
-          };
-          x.send();
     }
